3ef691973d5cc3df80c97e1a2b66d5215c5f2830
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-03-07 15:38:14 -0600

    added model for add job form

--- src/components/Job/JobCreate/JobCreate.jsx
+++ src/components/Job/JobCreate/JobCreate.jsx
@@ -1,45 +1,12 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-// React components
-
-// MUI imports
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Modal from "@mui/material/Modal";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import CircularProgress from "@mui/material/CircularProgress";
 
 function JobCreate({ job }) {
   // Dispatch hook
   const dispatch = useDispatch();
 
-  //  MUI modal setup for detail view
-  const [open, setOpen] = useState(false);
-  const handleClose = () => {
-    setOpen(false);
-  };
-  // Modal style setup
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    bgcolor: "background.paper",
-    boxShadow: 24,
-    width: "80%",
-    height: "80%",
-    p: 4,
-    overflow: "scroll",
-    padding: 3,
-  };
+
 
   return <h1>hello</h1>;
 }
