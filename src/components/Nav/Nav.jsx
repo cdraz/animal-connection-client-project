d6e91b864587cd26acf6fe8962d5722374e0cbe7
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-03-07 13:13:39 -0600

    a

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -28,6 +28,10 @@ function Nav() {
               Home
             </Link>
 
+            <Link className="navLink" to="/animals">
+              Animals
+            </Link>
+
             <Link className="navLink" to="/info">
               Info Page
             </Link>
