5e53f7401729dea4dcb3d56f0a5bbec324edbc6d
Christopher Razidlo <cd.razidlo@gmail.com>
2022-03-08 11:55:52 -0600

    reworked selectedAnimalReducer to have 4 reducers for details, work history, audition history, contact info

--- src/redux/reducers/selectedAnimal.reducer.js
+++ src/redux/reducers/selectedAnimal.reducer.js
@@ -1,4 +1,6 @@
-const selectedAnimalReducer = (state = {}, action) => {
+import { combineReducers } from 'redux';
+
+const animalDetails = (state = {}, action) => {
     switch (action.type) {
         case 'SET_SELECTED_ANIMAL':
             return action.payload[0];
@@ -6,5 +8,39 @@ const selectedAnimalReducer = (state = {}, action) => {
             return state;
     }
 };
+
+const workHistory = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_SELECTED_ANIMAL_WORK_HISTORY':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const auditionHistory = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_SELECTED_ANIMAL_AUDITION_HISTORY':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const animalContactInfo = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_SELECTED_ANIMAL_CONTACT_INFO':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+const selectedAnimalReducer = combineReducers({
+    animalDetails,
+    workHistory,
+    auditionHistory,
+    animalContactInfo
+});
 
 export default selectedAnimalReducer;
