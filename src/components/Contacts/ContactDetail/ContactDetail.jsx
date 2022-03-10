aa955c947e41b8ca566b773efe65037183c8fb15
Subash Khatri <subashkhatri44@gmail.com>
2022-03-10 16:03:12 -0600

    conatct goes to detail page but does not render yet

--- src/components/Contacts/ContactDetail/ContactDetail.jsx
+++ src/components/Contacts/ContactDetail/ContactDetail.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import {useHistory} from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import EditSharpIcon from '@mui/icons-material/EditSharp';
+import Button from "@mui/material/Button";
 
 
-function contactDetail() {
+function ContactDetail() {
     const dispatch = useDispatch();
     const history =  useHistory();
     const contacts = useSelector(store => store.contact)
@@ -14,12 +15,21 @@ function contactDetail() {
 
     return (
         <>
-        <h5 className="contact">Contact List <span>
-        <EditSharpIcon onClick={() => history.push('/contact-edit')}>Edit</EditSharpIcon></span></h5>
         <p className="contact-detail">Type: {`${contacts.type}`} Name: {`${contacts.firstName} `}</p>
+        <EditSharpIcon onClick={() => history.push('/contact-edit')}>Edit</EditSharpIcon>
+
+        {/* <Button
+            onClick={deleteJob}
+            id="jobDelete"
+            type="button"
+            value="Delete"
+            variant="contained"
+          >
+            Delete Job
+          </Button> */}
 
         </>
     )
 };
 
-export  default contactDetail;
+export  default ContactDetail;
