ce77d596fd10e798bf69a71591c273c0feaf0d58
Subash Khatri <subashkhatri44@gmail.com>
2022-03-17 15:46:25 -0500

    fixing job card edit css

---
+++ src/components/Contacts/ContactPage/ContactPage 2.jsx
@@ -0,0 +1,19 @@
+// Function imports
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function ContactPage() {
+    // Dispatch hook, store access
+    const dispatch = useDispatch();
+    
+  
+  
+      return(
+          <>
+          
+        </>
+      )
+  }
+  
+  export default ContactPage;
+  
